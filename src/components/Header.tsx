2f1f301e1b40ae38228aa4f1aae7df284d4563ab
temari1013 <smile21512211@icloud.com>
2025-04-02 12:01:38 +0900

    下線をdiv要素で作ると当たり判定で困るのでafterを使うように修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import  './src/styles/global.css';
 import {HeaderButton} from '../components/HeaderButton.tsx';
 
+
 export const Header = () => {
   return (
     <div
--- src/components/HeaderButton.tsx
+++ src/components/HeaderButton.tsx
@@ -3,40 +3,20 @@ import React from "react";
 import type {FC} from "react";
 import type { ButtonHTMLAttributes } from "react";
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  color?: "bg_white" | "bg_red" ; 
-   children: React.ReactNode;
-   className?: string
-}
-
 export const HeaderButton: FC<ButtonProps> = ({
-  color = "bg_white",
+
   children,
   ...props
 }) => {
-  let buttonStyle ="bg_white";
-  let underBarStyle= "";
-  if (color === "bg_white"){
- buttonStyle = " bg-white text-[#E93A3A]  bordercolor-[#E93A3A]  "//ここにボタンのcssの差分を記述
-  }else if (color === "bg_red"){
-    buttonStyle = "bg-[#E93A3A] text-white " ;//ここにボタンのcssの差分を記述
-  }
-
+ 
   return (
-    <button>
-     <div className = {`${buttonStyle} 
-font-[Jaro] 
-rounded-lg
- px-2 py-1
-text-xl 
-hover:opacity-60 `}  {...props}
->
+    <button className ="font-[Jaro]  text-xl bg-white text-[#E93A3A] hover:text-white relative 
+                 hover:bg-[#E93A3A] hover:text-white rounded-xl px-2 py-1
+                 after:content-[''] after:absolute after:left-1/2 after:bottom-0
+                 after:w-[75%] after:h-[6px] after:bg-[#E93A3A] after:translate-x-[-50%]" >
+     <div className="px-2 py-1" {...props}>
       {children}
     </div>
- { color === 'bg_white' && (
-    <div className="w-[80%] border-b-6 text-[#E93A3A] mx-auto mt-0.50"></div>
-  )}
-
-    </button>
-  );
+       </button>
+  ); 
 };

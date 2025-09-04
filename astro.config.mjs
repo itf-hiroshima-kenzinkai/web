22d07e4d54c8593e6ff5c171694603f13e6e5d5f
temari1013 <smile21512211@icloud.com>
2025-09-04 19:55:17 +0900

    prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,12 @@
 import { defineConfig } from 'astro/config'
 
 import react from '@astrojs/react'
-import icon from 'astro-icon';
+import icon from 'astro-icon'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-    react(),
-    icon(),
-  ],
+  integrations: [react(), icon()],
   vite: {
     plugins: [tailwindcss()]
   }

4cef9b55fd298d706ed33207510b7ebc266785d3
temari1013 <smile21512211@icloud.com>
2025-12-30 16:46:02 +0900

    npm audit fix とフォントのセルフホスト化

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import react from '@astrojs/react'
 import icon from 'astro-icon'
 import tailwindcss from '@tailwindcss/vite'
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],

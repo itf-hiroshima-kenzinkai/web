278bd3218f2b9d55eaf8ea8989c131b0dc5b026f
ten1019 <170093316+N1019@users.noreply.github.com>
2025-12-30 21:17:00 +0900

    カルーセルへの画像追加

--- src/data/goods.ts
+++ src/data/goods.ts
@@ -3,37 +3,79 @@ import type { GoodsItem } from '../models/goods'
 export const goodsData: GoodsItem[] = [
   {
     id: 1,
-    imageUrl: '/ex1.png',
-    price: 22000,
-    description: 'test',
-    linkUrl: 'https://google.com'
+    imageUrl: '/goods_c1.png',
+    price: 2285,
+    description: 'キャラクターフェイスタオル',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19129953/face-towel/free/white'
   },
   {
     id: 2,
-    imageUrl: '/goods_ex1.webp',
-    price: 2500,
-    description: 'おこのみやき',
-    linkUrl: 'https://google.com'
+    imageUrl: '/goods_c2.png',
+    price: 1219,
+    description: '厳島神社タイポグラフィアクリルキーホルダー',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130402/acrylic-keychain/50x50mm/clear'
   },
   {
     id: 3,
-    imageUrl: '/goods_ex2.webp',
-    price: 3000,
-    description: 'カープ坊や',
-    linkUrl: 'https://google.com'
+    imageUrl: '/goods_c3.png',
+    price: 2079,
+    description: '厳島神社タイポグラフィマグカップ',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130409/mug/m/white'
   },
   {
     id: 4,
-    imageUrl: '/goos_ex3.webp',
-    price: 3000,
-    description: 'もみじまんじゅう',
-    linkUrl: 'https://google.com'
+    imageUrl: '/goods_c4.png',
+    price: 1012,
+    description: 'レモンタイポグラフィ缶バッジ',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130411/can-badge/75mm/white'
   },
   {
     id: 5,
-    imageUrl: '/019.webp',
-    price: 750,
-    description: 'くまのふで',
-    linkUrl: 'https://google.com'
+    imageUrl: '/goods_c5.png',
+    price: 732,
+    description: 'レモンタイポグラフィステッカー',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130313/sticker/m/white'
+  },
+  {
+    id: 6,
+    imageUrl: '/goods_c6.png',
+    price: 1219,
+    description: '会員風アクリルキーホルダー',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130312/acrylic-keychain/50x50mm/clear'
+  },
+  {
+    id: 7,
+    imageUrl: '/goods_c7.png',
+    price: 2079,
+    description: '会員風マグカップ',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130308/mug/m/white'
+  },
+  {
+    id: 8,
+    imageUrl: '/goods_c8.png',
+    price: 1012,
+    description: '会員風缶バッジ',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130310/can-badge/75mm/white'
+  },
+  {
+    id: 9,
+    imageUrl: '/goods_c9.png',
+    price: 732,
+    description: '会員風ステッカー',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130370/sticker/m/white'
+  },
+  {
+    id: 10,
+    imageUrl: '/goods_c10.png',
+    price: 1219,
+    description: '会員風アクリルキーホルダー',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130383/acrylic-keychain/50x50mm/clear'
+  },
+  {
+    id: 11,
+    imageUrl: '/goods_c11.png',
+    price: 2079,
+    description: '会員風マグカップ',
+    linkUrl: 'https://suzuri.jp/Hiroshima_kenjinkai_UnivTsukuba/19130375/mug/m/white'
   }
 ]

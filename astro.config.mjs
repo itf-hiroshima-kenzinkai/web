69b530ddfa7cb2d8c2afc841ffda24e243e6dfd3
temari1013 <smile21512211@icloud.com>
2025-12-30 17:04:57 +0900

    再調整

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import react from '@astrojs/react'
 import icon from 'astro-icon'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), icon()],
